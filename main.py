f705d8905233b88bcc38b5021a6706812d4e58a5
g-abilio <gabrielabiliobferreira2@gmail.com>
2025-09-29 21:28:41 -0300

    testing TestCase class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from TestCase import TestCase
 from TestResult import TestResult
+from testing import TestCaseTest
 
 class MyTest(TestCase): 
     def set_up(self): 
@@ -17,7 +18,7 @@ class MyTest(TestCase):
     def test_c(self): 
         print("test_c")
 
-def main(): 
+def MyTest_usage(): 
     result = TestResult()
 
     test = MyTest("test_a")
@@ -31,4 +32,33 @@ def main():
 
     print(result.summary())
 
-main()
+def TestCaseTest_usage(): 
+    result = TestResult()
+
+    test = TestCaseTest.TestCaseTest("test_result_success_run")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_result_failure_run")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_result_error_run")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_result_multiple_run")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_was_set_up")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_was_run")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_was_tear_down")
+    test.run(result)
+
+    test = TestCaseTest.TestCaseTest("test_template_method")
+    test.run(result)
+
+    print(result.summary())
+
+TestCaseTest_usage()
